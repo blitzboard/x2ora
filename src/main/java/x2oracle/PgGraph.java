a7c73bf96101b570a9736d5fad4ce628f4830709
ryotayamanaka <ryotayamanaka@gmail.com>
2020-05-26 18:36:17 +0900

    node id as long

--- src/main/java/x2oracle/PgGraph.java
+++ src/main/java/x2oracle/PgGraph.java
@@ -8,12 +8,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 class PgGraph {
   public String name;
-  public Map<String, PgNode> nodes = new HashMap<>();
+  public Map<Long, PgNode> nodes = new HashMap<>();
   public List<PgEdge> edges = new ArrayList<PgEdge>();
   public void setName(String name) {
     this.name = name;
   }
-  public void addNode(String id, PgNode node) {
+  public void addNode(long id, PgNode node) {
     nodes.put(id, node);
   }
   public void addEdge(PgEdge edge) {
@@ -32,10 +32,10 @@ class PgGraph {
 }
 
 class PgNode {
-  public String id;
+  public long id;
   public Set<String> labels = new HashSet<String>();
   public Map<String, String> properties = new HashMap<>();
-  public PgNode(String id) {
+  public PgNode(long id) {
     this.id = id;
   }
   public void addLabel(String label) {
@@ -47,12 +47,12 @@ class PgNode {
 }
 
 class PgEdge {
-  public String id1;
-  public String id2;
+  public long id1;
+  public long id2;
   public boolean undirected = false;
   public Set<String> labels = new HashSet<String>();
   public Map<String, String> properties = new HashMap<>();
-  public PgEdge(String id1, String id2, boolean undirected) {
+  public PgEdge(long id1, long id2, boolean undirected) {
     this.id1 = id1;
     this.id2 = id2;
     this.undirected = undirected;
--- src/main/java/x2oracle/Test.java
+++ src/main/java/x2oracle/Test.java
@@ -41,13 +41,15 @@ public class Test {
 			
 			String query;
 			PgqlResultSet rs;
+			
+			long node_id = Long.parseLong(node_ids);
 
 			switch (endpoint) {
 			
 			case "node_match":
-				query = "SELECT n, m, e MATCH (n)-[e]->(m) WHERE ID(n) = " + node_ids;
+				query = "SELECT n, m, e MATCH (n)-[e]->(m) WHERE ID(n) = " + node_id;
 				rs = graph.queryPgql(query);
-				result = getResultPG(rs, 2, 1, 0, 0, node_ids);
+				result = getResultPG(rs, 2, 1, 0, 0, node_id);
 				break;
 			
 			case "traversal":
@@ -59,17 +61,17 @@ public class Test {
 						+ " ID(n4) AS e5s, ID(n4) AS e5d,"
 						+ " ID(n5) AS e6s, ID(n5) AS e6d"
 						+ " MATCH (n)-[e1]->(n1)-[e2]->(n2)-[e3]->(n3)-[e4]->(n4)-[e5]->(n5)-[e6]->(n6)"
-						+ " WHERE ID(n) = " + node_ids;
+						+ " WHERE ID(n) = " + node_id;
 				rs = graph.queryPgql(query);
-				result = getResultPG(rs, 7, 6, 0, 0, node_ids);
+				result = getResultPG(rs, 7, 6, 0, 0, node_id);
 				break;
 
 			case "cycle":
 				query = "SELECT ID(n), ARRAY_AGG(ID(m)), ARRAY_AGG(ID(e))"
 				+ " MATCH TOP 2 SHORTEST ((n) (-[e:transfer]->(m))* (n)) WHERE ID(n) = "
-				+ node_ids;
+				+ node_id;
 				rs = graph.queryPgql(query);
-				result = getResultPG(rs, 1, 0, 1, 1, node_ids);
+				result = getResultPG(rs, 1, 0, 1, 1, node_id);
 				break;
 			
 			case "query":
@@ -87,24 +89,25 @@ public class Test {
 		return result;		
 	}
 	
-	private static String getResultPG(PgqlResultSet rs, int cnt_v, int cnt_e, int cnt_vl, int cnt_el, String node_ids) {
+	private static String getResultPG(PgqlResultSet rs, int cnt_v, int cnt_e, int cnt_vl, int cnt_el, long node_id) {
 		PgGraph pg = new PgGraph();
 		pg.setName("test_graph");
 		try {
 			while (rs.next()) {
 				for (int i = 1; i <= cnt_v; i++) {
-					addNodeById(pg, rs.getInteger(i).toString());
+					addNodeById(pg, rs.getLong(i));
 				}
 				for (int i = cnt_v + 1; i <= cnt_v + (cnt_e * 2); i = i + 2) {
 					//addEdge(pg, rs.getEdge(i));
-					addEdgeByIds(pg, rs.getInteger(i).toString(), rs.getInteger(i + 1).toString(), "transfer");
+					addEdgeByIds(pg, rs.getLong(i), rs.getLong(i + 1), "transfer");
 				}
 				for (int i = cnt_v + (cnt_e * 2) + 1; i <= cnt_v + (cnt_e * 2) + cnt_vl; i++) {
 					if(rs.getList(i) != null) {
-						String node_src = node_ids;
+						long node_src = node_id;
+						long node_dst;
 						for (Object nodeId : rs.getList(i)) {
-							String node_dst = nodeId.toString();
-							addNodeById(pg, nodeId.toString());
+							node_dst = (long) nodeId;
+							addNodeById(pg, node_dst);
 							addEdgeByIds(pg, node_src, node_dst, "transfer");
 							node_src = node_dst;
 						}
@@ -126,27 +129,32 @@ public class Test {
 		return sw.toString();
 	}
   
+	/*
 	private static void addNode(PgGraph pg, PgxVertex<?> v) {
-		String id = v.getId().toString();
+		long id = (long) v.getId();
+		PgNode node = new PgNode(id);
+		pg.addNode(id, node);
+	}
+	*/
+
+	private static void addNodeById(PgGraph pg, long id) {
 		PgNode node = new PgNode(id);
 		pg.addNode(id, node);
 	}
 
-	private static void addNodeById(PgGraph pg, String id) {
-		PgNode node = new PgNode(id);
-		pg.addNode(id, node);
-	}
-
+	/*
 	private static void addEdge(PgGraph pg, PgxEdge e) {
 		PgEdge edge = new PgEdge(
-				e.getSource().getId().toString(),
-				e.getDestination().getId().toString(),
+				(long) e.getSource().getId(),
+				(long) e.getDestination().getId(),
 				false
 				);
 		edge.addLabel(e.getLabel());
 		pg.addEdge(edge);
 	}
-	private static void addEdgeByIds(PgGraph pg, String id_s, String id_d, String label) {
+	*/
+	
+	private static void addEdgeByIds(PgGraph pg, long id_s, long id_d, String label) {
 		PgEdge edge = new PgEdge(
 				id_s,
 				id_d,
