0802bae74263fe92d896b4cc1de3de4c02a9b67f
ryotayamanaka <ryotayamanaka@gmail.com>
2021-05-09 21:17:52 +0900

    Use of properties file

---
+++ src/main/java/x2oracle/Main.java
@@ -0,0 +1,44 @@
+package x2oracle;
+
+import x2oracle.UpdateController;
+import io.javalin.Javalin;
+import oracle.pg.rdbms.pgql.jdbc.PgqlJdbcRdbmsDriver;
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.util.ResourceBundle;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+public class Main {
+
+	public static Connection conn;
+
+	public static void main(String[] args) throws Exception {
+		Javalin app = Javalin.create(config -> {
+			config.enableCorsForAllOrigins();
+		}).start(7000);
+		
+		ResourceBundle rb = ResourceBundle.getBundle("common");
+		DriverManager.registerDriver(new PgqlJdbcRdbmsDriver());
+		conn = DriverManager.getConnection(
+			rb.getString("jdbc_url"),
+			rb.getString("username"),
+			rb.getString("password")
+			);
+		conn.setAutoCommit(false);
+
+		System.out.println("INFO: Ready to accept requests");
+		app.get("/merge_node/", UpdateController.mergeNode); 
+		app.get("/merge_edge/", UpdateController.mergeEdge);
+	}
+
+	public static String printException(Exception e) {
+		e.printStackTrace();
+		StringWriter sw = new StringWriter();
+		PrintWriter pw = new PrintWriter(sw);
+		e.printStackTrace(pw);
+		pw.flush();
+		return sw.toString();
+	}
+
+}
