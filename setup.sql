5b07216f39470efbbe599b05cc43f2bad304b916
ryotayamanaka <ryota.yamanaka@oracle.com>
2022-10-03 15:39:44 +0900

    BLOB for JSON columns

--- setup.sql
+++ setup.sql
@@ -1,4 +1,4 @@
--- SQL
+-- SQL (CLOB and VARCHAR2)
 
 CREATE TABLE x2graph (
   id VARCHAR2(255)
@@ -30,6 +30,40 @@ CREATE TABLE x2edge (
 , CONSTRAINT x2edge_fk_dst FOREIGN KEY (graph, dst) REFERENCES x2node(graph, id)
 , CONSTRAINT x2edge_check CHECK (props IS JSON)
 );
+
+-- SQL (BLOB)
+
+CREATE TABLE x2graph (
+  id VARCHAR2(255)
+, props BLOB
+, CONSTRAINT x2graph_pk PRIMARY KEY (id)
+, CONSTRAINT x2graph_check CHECK (props IS JSON FORMAT OSON)
+);
+
+CREATE TABLE x2node (
+  graph VARCHAR2(255)
+, id VARCHAR2(255)
+, label VARCHAR2(255)
+, props BLOB
+, CONSTRAINT x2node_pk PRIMARY KEY (graph, id)
+, CONSTRAINT x2node_fk_graph FOREIGN KEY (graph) REFERENCES x2graph(id)
+, CONSTRAINT x2node_check CHECK (props IS JSON FORMAT OSON)
+);
+
+CREATE TABLE x2edge (
+  graph VARCHAR2(255)
+, id VARCHAR2(255)
+, src VARCHAR2(255)
+, dst VARCHAR2(255)
+, label VARCHAR2(255)
+, props BLOB
+, CONSTRAINT x2edge_pk PRIMARY KEY (graph, id)
+, CONSTRAINT x2edge_fk_graph FOREIGN KEY (graph) REFERENCES x2graph(id)
+, CONSTRAINT x2edge_fk_src FOREIGN KEY (graph, src) REFERENCES x2node(graph, id)
+, CONSTRAINT x2edge_fk_dst FOREIGN KEY (graph, dst) REFERENCES x2node(graph, id)
+, CONSTRAINT x2edge_check CHECK (props IS JSON FORMAT OSON)
+);
+
 
 INSERT INTO x2node VALUES ('TEST', '1', 'PERSON', '{"AGE":[37]}');
 INSERT INTO x2node VALUES ('TEST', '2', 'PERSON', '{"AGE":[36]}');
