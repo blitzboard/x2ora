2ced4c031374a5444b7ff00239649bfcef1ad23d
ryotayamanaka <ryota.yamanaka@oracle.com>
2022-10-08 18:10:54 +0900

    Removed PGQL from UpdateController

--- src/main/java/x2ora/UpdateController.java
+++ src/main/java/x2ora/UpdateController.java
@@ -8,11 +8,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import oracle.pg.rdbms.pgql.PgqlConnection;
-import oracle.pg.rdbms.pgql.PgqlResultSet;
-import oracle.pgql.lang.PgqlException;
-import oracle.pg.rdbms.pgql.PgqlPreparedStatement;
-
 import java.util.UUID;
 
 public class UpdateController {
@@ -226,136 +221,123 @@ public class UpdateController {
 
   private static String mergeNode(String strGraph, String strId, String strLabel, String strProps) throws SQLException {
     String result = "";
-    try {
-      Boolean able = true;
-
-      // Check if the node exists
-      if (able) {
-        PgqlConnection pgqlConn = PgqlConnection.getConnection(conn);
-        PgqlPreparedStatement pps = pgqlConn.prepareStatement("SELECT v.id FROM MATCH (v) ON " + strPgv + " WHERE v.graph = ? AND v.label = ? AND v.id = ?");
-        pps.setString(1, strGraph);
-        pps.setString(2, strLabel.toUpperCase());
-        pps.setString(3, strId);
-        pps.execute();
-        PgqlResultSet rs = pps.getResultSet();
-        if (rs.first()){
+    Boolean able = true;
+    // Check if the node exists
+    if (able) {
+      try {
+        String query = "SELECT id FROM " + strPgvNode + " WHERE graph = ? AND label = ? AND id = ?";
+        PreparedStatement ps = conn.prepareStatement(query);
+        ps.setString(1, strGraph);
+        ps.setString(2, strLabel.toUpperCase());
+        ps.setString(3, strId);
+        ResultSet rs = ps.executeQuery();
+        if (rs.next()){
           able = false;
           result = "Node " + strLabel.toUpperCase() + " " + strId + " exists.";
         }
         rs.close();
-        pps.close();
+        ps.close();
+      } catch (SQLException e) {
+        result = printException(e);
       }
-
-      // Insert the node if not exists
-      if (able) {
-        String query = "INSERT INTO " + strPgvNode + " VALUES (?, ?, ?, ?)";			
-        try (PreparedStatement ps = conn.prepareStatement(query)) {
-          ps.setString(1, strGraph);
-          ps.setString(2, strId);
-          ps.setString(3, strLabel.toUpperCase());
-          ps.setString(4, strProps);
-          ps.execute();
-          result = "Node " + strLabel.toUpperCase() + " " + strId + " is added.";  
-          ps.close();
-        } catch (Exception e) {
-          conn.rollback();
-          System.out.println("rollback");
-          throw e;
-        };
-      } else {
-        // The update query below gets PgqlToSqlException so this operation is skipped now
-        /*
-        String query = "UPDATE v SET (v.json = ?) FROM MATCH (v) ON " + strLabel + " WHERE LABEL(v) = ? AND v.id = ?";			
-        System.out.println(query);
-        ps = pgqlConn.prepareStatement(query);
-        ps.setString(1, strProps);
-        ps.setString(2, strLabel.toUpperCase());
-        ps.setString(3, strId);
+    }
+    // Insert the node if not exists
+    if (able) {
+      String query = "INSERT INTO " + strPgvNode + " VALUES (?, ?, ?, ?)";
+      try (PreparedStatement ps = conn.prepareStatement(query)) {
+        ps.setString(1, strGraph);
+        ps.setString(2, strId);
+        ps.setString(3, strLabel.toUpperCase());
+        ps.setString(4, strProps);
         ps.execute();
-        result = "Node " + strLabel + " " + strId + " is update.";
-        */
-      }
-      
-    } catch (PgqlException e) {
-      result = printException(e);
+        result = "Node " + strLabel.toUpperCase() + " " + strId + " is added.";
+        ps.close();
+      } catch (Exception e) {
+        conn.rollback();
+        System.out.println("rollback");
+        result = printException(e);
+      };
     }
     return result;
   };
 
   private static String mergeEdge(String strGraph, String strSrcId, String strDstId, String strLabel, String strProps) throws SQLException {
     String result = "";
-    try {
-      
-      Boolean able = true;
-      PgqlConnection pgqlConn = PgqlConnection.getConnection(conn);
-
-      // Check if the node exists
-      if (able) {
-        PgqlPreparedStatement pps = pgqlConn.prepareStatement("SELECT v.id FROM MATCH (v) ON " + strPgv + " WHERE v.graph = ? AND v.id = ?");
-        pps.setString(1, strGraph);
-        pps.setString(2, strSrcId);
-        pps.execute();
-        PgqlResultSet rs = pps.getResultSet();
-        if (!rs.first()){
+    Boolean able = true;
+    // Check if the source node exists
+    if (able) {
+      try {
+        String query = "SELECT id FROM " + strPgvNode + " WHERE graph = ? AND id = ?";
+        PreparedStatement ps = conn.prepareStatement(query);
+        ps.setString(1, strGraph);
+        ps.setString(2, strSrcId);
+        ResultSet rs = ps.executeQuery();
+        if (!rs.next()){
           able = false;
-          result = "Node " + strSrcId + " does not exist.";
-        }
-      }
-
-      // Check if the node exists
-      if (able) {
-        PgqlPreparedStatement pps = pgqlConn.prepareStatement("SELECT v.id FROM MATCH (v) ON " + strPgv + " WHERE v.graph = ? AND v.id = ?");
-        pps.setString(1, strGraph);
-        pps.setString(2, strDstId);
-        pps.execute();
-        PgqlResultSet rs = pps.getResultSet();
-        if (!rs.first()){
-          able = false;
-          result = "Node " + strDstId + " does not exist.";
+          result = "Node " + strLabel.toUpperCase() + " " + strSrcId + " does not exist.";
         }
         rs.close();
-        pps.close();
+        ps.close();
+      } catch (SQLException e) {
+        result = printException(e);
       }
-
-      // Check if the edge exists
-      if (able) {
-        PgqlPreparedStatement pps = pgqlConn.prepareStatement("SELECT e.id FROM MATCH (src)-[e]->(dst) ON " + strPgv + " WHERE e.graph = ? AND e.label = ? AND src.id = ? AND dst.id = ?");
-        pps.setString(1, strGraph);
-        pps.setString(2, strLabel.toUpperCase());
-        pps.setString(3, strSrcId);
-        pps.setString(4, strDstId);
-        pps.execute();
-        PgqlResultSet rs = pps.getResultSet();
-        if (rs.first()){
+    }
+    // Check if the destination node exists
+    if (able) {
+      try {
+        String query = "SELECT id FROM " + strPgvNode + " WHERE graph = ? AND id = ?";
+        PreparedStatement ps = conn.prepareStatement(query);
+        ps.setString(1, strGraph);
+        ps.setString(2, strDstId);
+        ResultSet rs = ps.executeQuery();
+        if (!rs.next()){
           able = false;
-          result = "Edge " + strLabel + " " + strSrcId + " -> " + strDstId + " exists.";
+          result = "Node " + strLabel.toUpperCase() + " " + strDstId + " doest not exist.";
         }
         rs.close();
-        pps.close();
+        ps.close();
+      } catch (SQLException e) {
+        result = printException(e);
       }
-
-      // Insert the edge if not exists
-      if (able) {
-        String query = "INSERT INTO " + strPgvEdge + " VALUES (?, ?, ?, ?, ?, ?)";			
-        try (PreparedStatement ps = conn.prepareStatement(query)) {
-          ps.setString(1, strGraph);
-          ps.setString(2, UUID.randomUUID().toString());
-          ps.setString(3, strSrcId);
-          ps.setString(4, strDstId);
-          ps.setString(5, strLabel);
-          ps.setString(6, strProps);
-          ps.execute();
-          result = "Edge " + strLabel + " " + strSrcId + " -> " + strDstId + " is added.";
-          ps.close();
-        } catch (Exception e) {
-          conn.rollback();
-          System.out.println("rollback");
-          throw e;
-        };
+    }
+    // Check if the edge exists
+    if (able) {
+      try {
+        String query = "SELECT id FROM " + strPgvEdge + " WHERE graph = ? AND label = ? AND src = ? AND dst = ?";
+        PreparedStatement ps = conn.prepareStatement(query);
+        ps.setString(1, strGraph);
+        ps.setString(2, strLabel.toUpperCase());
+        ps.setString(3, strSrcId);
+        ps.setString(4, strDstId);
+        ResultSet rs = ps.executeQuery();
+        if (rs.next()){
+          able = false;
+          result = "Edge " + strLabel.toUpperCase() + " " + strSrcId + " -> " + strDstId + " exists.";
+        }
+        rs.close();
+        ps.close();
+      } catch (SQLException e) {
+        result = printException(e);
       }
-      
-    } catch (PgqlException e) {
-      result = printException(e);
+    }
+    // Insert the edge if not exists
+    if (able) {
+      String query = "INSERT INTO " + strPgvEdge + " VALUES (?, ?, ?, ?, ?, ?)";			
+      try (PreparedStatement ps = conn.prepareStatement(query)) {
+        ps.setString(1, strGraph);
+        ps.setString(2, UUID.randomUUID().toString());
+        ps.setString(3, strSrcId);
+        ps.setString(4, strDstId);
+        ps.setString(5, strLabel.toUpperCase());
+        ps.setString(6, strProps);
+        ps.execute();
+        result = "Edge " + strLabel.toUpperCase() + " " + strSrcId + " -> " + strDstId + " is added.";
+        ps.close();
+      } catch (Exception e) {
+        conn.rollback();
+        System.out.println("rollback");
+        throw e;
+      };
     }
     return result;
   };
