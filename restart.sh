ab1339ffacca51ea43e6a4e09b43ad3bb12844d5
ryotayamanaka <ryota.yamanaka@oracle.com>
2022-11-06 17:28:08 +0900

    Added restart.sh and logs

---
+++ restart.sh
@@ -0,0 +1,9 @@
+
+PID=`cat x2ora.pid`
+kill $PID
+TS=`date "+%Y%m%d-%H%M%S"`
+nohup ./gradlew run > logs/$TS.log  &
+ln -sf $TS.log logs/latest.log
+pid=$!
+echo $pid > x2ora.pid
+tail -f logs/latest.log
