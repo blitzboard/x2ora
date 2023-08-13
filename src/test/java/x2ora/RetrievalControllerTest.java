f9b5db5df735d3f6d03bc4bc8de8e97dc363d87d
ryotayamanaka <ryota.yamanaka@oracle.com>
2023-08-13 16:38:26 +0900

    Add a test

--- src/test/java/x2ora/RetrievalControllerTest.java
+++ src/test/java/x2ora/RetrievalControllerTest.java
@@ -1,31 +1,24 @@
 package x2ora;
 
-import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.Assertions.*;
-
-import io.javalin.Javalin;
-import io.javalin.testtools.JavalinTest;
+import io.javalin.http.BadRequestResponse;
+import io.javalin.http.Context;
+import org.junit.Test;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
 
 public class RetrievalControllerTest {
 
-    /*
-    Javalin app = Javalin.create();
-    
-    @Test
-    public void testList() {
-        JavalinTest.test(app, (server, client) -> {
-            assertThat(client.get("/list").code()).isEqualTo(200);
-        });
-    }
-    */
+  private final Context ctx = mock(Context.class);
 
-    /*
-    @Test
-    public void testList() {
-        JavalinTest.test((app, client) -> {
-            app.get("/list/", RetrievalController.list);
-            assertThat(client.get("/list/").code()).isEqualTo(200);
-        });
+  @Test
+  public void POST_to_list_graphs_gives_201() {
+    try {
+        RetrievalController.list.handle(ctx);
+    } catch (Exception e) {
+        e.printStackTrace();
     }
-    */
+    verify(ctx).status(201);
+  }
+
 }
